dd40fd3061f147951546b96042c141b4f92b4485
ist1108469 <francisco.r.nogueira@tecnico.ulisboa.pt>
2024-05-16 14:14:57 +0100

    Write to metrics

--- webserver/src/main/java/pt/ulisboa/tecnico/cnv/webserver/MetricsMiddleware.java
+++ webserver/src/main/java/pt/ulisboa/tecnico/cnv/webserver/MetricsMiddleware.java
@@ -7,8 +7,23 @@ import java.io.IOException;
 public class MetricsMiddleware implements HttpHandler {
     private HttpHandler handler;
 
+    public static final String METRICS_FILE = "metrics.log";
+
     public MetricsMiddleware(HttpHandler handler) {
         this.handler = handler;
+    }
+
+    private void appendMetricsToFile(RequestMetrics metrics) throws IOException {
+        // Open the metrics file
+        java.io.FileWriter fileWriter = new java.io.FileWriter(METRICS_FILE, true);
+        java.io.BufferedWriter bufferedWriter = new java.io.BufferedWriter(fileWriter);
+
+        // Write the metrics to the file
+        bufferedWriter.write(metrics.toString());
+        bufferedWriter.newLine();
+
+        // Close the file
+        bufferedWriter.close();
     }
 
     @Override
@@ -19,6 +34,10 @@ public class MetricsMiddleware implements HttpHandler {
         } finally {
             MetricsContext.end();
             RequestMetrics metrics = MetricsContext.getMetrics();
+
+            // Append the metrics to the metrics file
+            appendMetricsToFile(metrics);
+
             System.out.println("Metrics: " + metrics.toJSON());
         }
     }
