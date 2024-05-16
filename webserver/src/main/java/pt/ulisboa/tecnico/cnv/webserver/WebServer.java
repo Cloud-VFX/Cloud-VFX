44692d5b388eb3ba541d0d743f6f6428ce58adb6
Davi Giordano Valerio <davi.giordano@tecnico.ulisboa.pt>
2024-05-16 20:34:29 +0000

    Revert "Merge branch 'master' of gitlab.rnl.tecnico.ulisboa.pt:cnv/cnv24-groups/cnv24-g02"
    This reverts commit a63fc4672d0a335e550c96cb7ae5955ab5ba4f32

--- webserver/src/main/java/pt/ulisboa/tecnico/cnv/webserver/WebServer.java
+++ webserver/src/main/java/pt/ulisboa/tecnico/cnv/webserver/WebServer.java
@@ -1,6 +1,5 @@
 package pt.ulisboa.tecnico.cnv.webserver;
 
-import java.io.IOException;
 import java.net.InetSocketAddress;
 import com.sun.net.httpserver.HttpServer;
 
@@ -9,30 +8,14 @@ import pt.ulisboa.tecnico.cnv.imageproc.EnhanceImageHandler;
 import pt.ulisboa.tecnico.cnv.raytracer.RaytracerHandler;
 
 public class WebServer {
-
-    public static final String METRICS_FILE = "metrics.log";
-
-    private static void createMetricsFile() throws IOException {
-        java.io.File file = new java.io.File(METRICS_FILE);
-        // Check if the file already exists
-        if (file.exists()) {
-            System.out.println("Metrics file already exists.");
-            return;
-        }
-        file.createNewFile();
-    }
-
     public static void main(String[] args) throws Exception {
         System.out.println("Starting WebServer...");
-        createMetricsFile();
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
         server.setExecutor(java.util.concurrent.Executors.newCachedThreadPool());
-
-        server.createContext("/", new MetricsMiddleware(new RootHandler()));
-        server.createContext("/raytracer", new MetricsMiddleware(new RaytracerHandler()));
-        server.createContext("/blurimage", new MetricsMiddleware(new BlurImageHandler()));
-        server.createContext("/enhanceimage", new MetricsMiddleware(new EnhanceImageHandler()));
-
+        server.createContext("/", new RootHandler());
+        server.createContext("/raytracer", new RaytracerHandler());
+        server.createContext("/blurimage", new BlurImageHandler());
+        server.createContext("/enhanceimage", new EnhanceImageHandler());
         System.out.println("WebServer started on port 8000");
         server.start();
     }
