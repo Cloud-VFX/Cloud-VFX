e046bcfc3d0b26e4adbdd12a0252b83e0f9c4926
Davi Giordano <davi.giordano@tecnico.ulisboa.pt>
2024-05-30 08:09:00 +0000

    Chore: Clean comments on App

--- LoadBalancerAutoScaler/src/main/java/pt/ulisboa/tecnico/cnv/App.java
+++ LoadBalancerAutoScaler/src/main/java/pt/ulisboa/tecnico/cnv/App.java
@@ -8,8 +8,8 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class App {
     public static void main(String[] args) throws Exception {
 
+        // TODO : remove later
         Dotenv dotenv = Dotenv.load();
-
         String keyName = dotenv.get("AWS_KEY_NAME");
         String securityGroup = dotenv.get("AWS_SECURITY_GROUP");
         String accessKey = dotenv.get("AWS_ACCESS_KEY_ID");
@@ -19,13 +19,11 @@ public class App {
         
         System.out.println("Testing env variables, accessKey: " + accessKey);
         
-        // Initialize AutoScaler
         AutoScaler autoScaler = new AutoScaler(accessKey, secretKey, amiId, instanceType, keyName, securityGroup);
         
-        // Initialize and start the LoadBalancer
-        LoadBalancer loadBalancer = new LoadBalancer(); // Assuming LoadBalancer does not require any arguments
-        int loadBalancerPort = 8080; // Choose an appropriate port for the load balancer
-        loadBalancer.start(loadBalancerPort); // Start the load balancer
+        LoadBalancer loadBalancer = new LoadBalancer();
+        int loadBalancerPort = 8080;
+        loadBalancer.start(loadBalancerPort);
 
         System.out.println("LoadBalancer is running on port " + loadBalancerPort);
     }
