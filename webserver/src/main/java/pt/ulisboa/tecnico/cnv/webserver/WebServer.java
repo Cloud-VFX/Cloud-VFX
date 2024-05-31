f638f58e0a96f852401228efd85a84d653c7f390
ist1108469 <francisco.r.nogueira@tecnico.ulisboa.pt>
2024-05-31 19:07:50 +0100

    Fix roothandler error

--- webserver/src/main/java/pt/ulisboa/tecnico/cnv/webserver/WebServer.java
+++ webserver/src/main/java/pt/ulisboa/tecnico/cnv/webserver/WebServer.java
@@ -7,22 +7,12 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 import com.sun.net.httpserver.HttpServer;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 import pt.ulisboa.tecnico.cnv.imageproc.BlurImageHandler;
 import pt.ulisboa.tecnico.cnv.imageproc.EnhanceImageHandler;
 import pt.ulisboa.tecnico.cnv.raytracer.RaytracerHandler;
-import pt.ulisboa.tecnico.cnv.metrics.AggregatedDAO;
 import pt.ulisboa.tecnico.cnv.metrics.AggregatedMetricsClient;
 import pt.ulisboa.tecnico.cnv.metrics.DynamoDBClient;
-import pt.ulisboa.tecnico.cnv.metrics.MetricsAggregator;
-
-import com.amazonaws.services.lambda.runtime.ClientContext;
-import com.amazonaws.services.lambda.runtime.CognitoIdentity;
-import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.LambdaLogger;
 
 public class WebServer {
 
@@ -47,7 +37,7 @@ public class WebServer {
     public static void main(String[] args) throws Exception {
         System.out.println("Starting WebServer...");
         createMetricsFile();
-        // startCpuUsagePublisher();
+        startCpuUsagePublisher();
 
         // Setup DynamoDB table
         DynamoDBClient.setupTable();
@@ -56,123 +46,15 @@ public class WebServer {
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
         server.setExecutor(java.util.concurrent.Executors.newCachedThreadPool());
 
-        server.createContext("/", new RootHandler());
+        server.createContext("/", new MetricsMiddleware(new RootHandler()));
         server.createContext("/raytracer", new MetricsMiddleware(new RaytracerHandler()));
         server.createContext("/blurimage", new MetricsMiddleware(new BlurImageHandler()));
         server.createContext("/enhanceimage", new MetricsMiddleware(new EnhanceImageHandler()));
-
-        // TODO: temporary endpoint to trigger aggregation
-        server.createContext("/aggregate", (exchange -> {
-            handleAggregation(new MetricsAggregator());
-            exchange.sendResponseHeaders(200, 0);
-            exchange.close();
-        }));
-
-        /**
-         * Endpoint to query the latest aggregated metrics for a specific metric type
-         * Usage: /query_aggregated?metricType=METRIC_TYPE
-         */
-        server.createContext("/query_aggregated", (exchange -> {
-            String query = exchange.getRequestURI().getQuery();
-            String[] params = query.split("=");
-            if (params.length != 2) {
-                exchange.sendResponseHeaders(400, 0);
-                exchange.close();
-                return;
-            }
-            String metricType = params[1];
-            AggregatedDAO dao = new AggregatedDAO();
-            String response = dao.readLatestAggregatedMetrics(metricType).toString();
-            exchange.sendResponseHeaders(200, response.length());
-            exchange.getResponseBody().write(response.getBytes());
-            exchange.close();
-
-        }));
 
         server.start();
 
         System.out.println("WebServer started on port 8000");
 
-        // Scheduled periodic aggregation of metrics
-        // TODO: This need to be moved to the LoadBalancer
-        // ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-        // MetricsAggregator aggregator = new MetricsAggregator();
-        // scheduler.scheduleAtFixedRate(() -> handleAggregation(aggregator), 10, 30,
-        // TimeUnit.SECONDS);
-
-    }
-
-    private static void handleAggregation(MetricsAggregator aggregator) {
-        try {
-            aggregator.handleRequest(null, new Context() {
-                @Override
-                public String getAwsRequestId() {
-                    return "localRequestId";
-                }
-
-                @Override
-                public String getLogGroupName() {
-                    return "localLogGroup";
-                }
-
-                @Override
-                public String getLogStreamName() {
-                    return "localLogStream";
-                }
-
-                @Override
-                public String getFunctionName() {
-                    return "localFunction";
-                }
-
-                @Override
-                public String getFunctionVersion() {
-                    return "1.0";
-                }
-
-                @Override
-                public String getInvokedFunctionArn() {
-                    return "localArn";
-                }
-
-                @Override
-                public CognitoIdentity getIdentity() {
-                    return null;
-                }
-
-                @Override
-                public ClientContext getClientContext() {
-                    return null;
-                }
-
-                @Override
-                public int getRemainingTimeInMillis() {
-                    return 10000;
-                }
-
-                @Override
-                public int getMemoryLimitInMB() {
-                    return 512;
-                }
-
-                @Override
-                public LambdaLogger getLogger() {
-                    return new LambdaLogger() {
-                        @Override
-                        public void log(String message) {
-                            System.out.println(message);
-                        }
-
-                        @Override
-                        public void log(byte[] message) {
-                            System.out.println(new String(message));
-                        }
-                    };
-                }
-            });
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
 }
